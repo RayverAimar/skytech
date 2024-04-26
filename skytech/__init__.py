df270fbd8a5673be64c8dc74f26e5c4713a508fa
Brianmax <george.maxi@ucsp.edu.pe>
2024-04-26 05:32:44 -0500

    feat: add friends

--- skytech/__init__.py
+++ skytech/__init__.py
@@ -21,14 +21,17 @@ class FlightModel(db.Model):
     scales = db.Column(db.Boolean, nullable=False)
 
 class User(db.Model):
+    # Modelo de usuario
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(50), nullable=False)
-    searches = relationship('SearchHistory', backref='user')
+    searches = db.relationship('SearchHistory', backref='user')
+    friend_requests_sent = db.relationship('FriendRequest', foreign_keys='FriendRequest.sender_id', backref='sender')
+    friend_requests_received = db.relationship('FriendRequest', foreign_keys='FriendRequest.receiver_id', backref='receiver')
 
 class SearchHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,7 +41,87 @@ class SearchHistory(db.Model):
     return_date = db.Column(db.String(50), nullable=False)
     usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+class FriendRequest(db.Model):
+    # Solicitud de amistad
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='pending')  # 'pending', 'accepted', 'rejected'
+
 #db.create_all()
+
+@app.route('/send_friend_request/<int:receiver_id>', methods=['POST'])
+def send_friend_request(receiver_id):
+    if 'user_id' in session:
+        sender_id = session['user_id']
+        friend_request = FriendRequest(sender_id=sender_id, receiver_id=receiver_id)
+        db.session.add(friend_request)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/accept_friend_request/<int:request_id>', methods=['POST'])
+def accept_friend_request(request_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        friend_request = FriendRequest.query.get(request_id)
+        if friend_request and friend_request.receiver_id == user_id:
+            friend_request.status = 'accepted'
+            db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/reject_friend_request/<int:request_id>', methods=['POST'])
+def reject_friend_request(request_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        friend_request = FriendRequest.query.get(request_id)
+        if friend_request and friend_request.receiver_id == user_id:
+            friend_request.status = 'rejected'
+            db.session.commit()
+    return redirect(url_for('index'))
+
+
+#def profile(user_id):
+#    if 'user_id' in session:
+#        user = User.query.get(user_id)
+#        if user:
+#            return render_template('profile.html', user=user)
+#    return redirect(url_for('index'))
+
+#@app.route('/profile', methods=['GET', 'POST'])
+@app.route('/profile/<int:user_id>', methods=['GET', 'POST'])
+def profile(user_id):
+    print('profile')
+    if 'user_id' in session:
+        user = User.query.get(user_id)
+        friend_requests_received = FriendRequest.query.filter_by(receiver_id=user_id, status='pending').all()
+        # find friends
+        friendsAccepted = FriendRequest.query.filter_by(sender_id=user_id, status='accepted').all()
+        friends = User.query.filter(User.id.in_([friend.receiver_id for friend in friendsAccepted])).all()
+        if request.method == 'POST':
+            sender_id = user.id
+            receiver_username = request.form['receiver_username']
+            receiver = User.query.filter_by(username=receiver_username).first()
+            if receiver:
+                # Verificar si ya existe una solicitud pendiente entre los usuarios
+                existing_request = FriendRequest.query.filter_by(sender_id=sender_id, receiver_id=receiver.id).first()
+                if not existing_request:
+                    new_request = FriendRequest(sender_id=sender_id, receiver_id=receiver.id)
+                    db.session.add(new_request)
+                    db.session.commit()
+        return render_template('profile.html', user=user, friend_requests_received=friend_requests_received, friends=friends)
+    else:
+        return redirect(url_for('login'))
+
+
+
+@app.route('/profile/search_users', methods=['GET', 'POST'])
+def search_users():
+    if request.method == 'POST':
+        search_query = request.form.get('search_query')
+        if search_query:
+            users = User.query.filter(User.username.ilike(f'%{search_query}%')).all()
+            return render_template('search_results.html', users=users)
+    return redirect(url_for('profile'))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
